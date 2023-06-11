5ce9111b2a720c3c1e1f3d1e394856817d29c8ee
Kyozen-1 <skadi1268@gmail.com>
2023-06-11 09:07:45 +0700

    done login

--- app/Http/Controllers/Auth/RazenBlog/LoginController.php
+++ app/Http/Controllers/Auth/RazenBlog/LoginController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\Auth\RazenBlog;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('guest:razen_blog')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.razen-blog.login');
+    }
+
+    public function login(Request $request)
+    {
+        // Validate the form data
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        // Attempt to log the user in
+        if (Auth::guard('razen_blog')->attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->intended(route('razen-blog.admin.dashboard.index'));
+        }
+        // if unsuccessful, then redirect back to the login with the form data
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function logout()
+    {
+        Auth::guard('razen_blog')->logout();
+        return redirect('/');
+    }
 }
--- routes/razen-blog.php
+++ routes/razen-blog.php
@@ -1,1 +1,11 @@
 <?php
+use App\Http\Controllers\RazenBlog\Admin\DashboardController;
+
+Route::prefix('razen-blog')->group(function(){
+    Route::prefix('admin')->group(function(){
+        Route::prefix('dashboard')->group(function(){
+            Route::get('/', [DashboardController::class, 'index'])->name('razen-blog.admin.dashboard.index');
+            Route::post('/change', [DashboardController::class, 'change'])->name('razen-blog.admin.dashboard.change');
+        });
+    });
+});
